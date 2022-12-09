69251bf2205a8c48910fce23f83f3f208398ec02
PreetS123 <preetbhardwaj517@gmail.com>
2022-12-09 10:22:32 +0530

    h1-tag-removed-from-app

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import { Main } from './Main';
 function App() {
   return (
     <div className='app'>
-      <h1>Tic Tac Toe Setup Ready</h1>
+      {/* <h1>Tic Tac Toe Setup Ready</h1> */}
       <Main/>
     </div>
   );
